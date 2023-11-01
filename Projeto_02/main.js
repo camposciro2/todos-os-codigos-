b2cda68d75d0f4554c8daf589b014b8ac6dbc3e8
camposciro2 <141172004+camposciro2@users.noreply.github.com>
2023-11-01 12:59:38 -0300

    Add files via upload

---
+++ Projeto_02/main.js
@@ -0,0 +1,65 @@
+const form = document.getElementById('form-atividade')
+const imgAprovado = '<img src="./Midia/aprovado.png" alt="Emoji celebrando">'
+const imgReprovado = '<img src="./Midia/reprovado.png" alt="Emoji descepção">'
+const atividades = []
+const notas = []
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
+const spanReprovado = '<span class="resultado Reprovado">Reprovado</span>'
+const notaMinima = parseFloat(prompt('Digite a nota minima:'))
+
+let linhas = ''
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    adicionarLinha()
+    atualizaTabela()
+    atualizaMediaFinal()
+})
+
+function adicionarLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade')
+    const inputNotaAtividade = document.getElementById('nota-atividade')
+
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A Atividade: ${inputNomeAtividade.value} já foi inserida!`)
+    } else {
+        atividades.push(inputNomeAtividade.value)
+        notas.push(parseFloat(inputNotaAtividade.value))
+
+        let linha = `<tr>`
+        linha += `<td>${inputNomeAtividade.value}</td>`
+        linha += `<td>${inputNotaAtividade.value}</td>`
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`
+        linha += `</tr>`
+
+        linhas += linha
+    }
+
+
+    inputNomeAtividade.value = ''
+    inputNotaAtividade.value = ''
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediasFinal()
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
+}
+
+function calculaMediasFinal() {
+    let somaDasNotas = 0
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+
+    return somaDasNotas / notas.length
+
+}
